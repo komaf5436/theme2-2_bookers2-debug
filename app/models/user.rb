d0bfe090fe41d9b953eb139e322e6f7729527d84
EC2 Default User <ec2-user@ip-172-31-2-101.ec2.internal>
2022-03-07 08:29:43 +0000

    [Add]follow/follower追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,12 +6,18 @@ class User < ApplicationRecord
 
   has_many :books, dependent: :destroy
   has_many :favorites, dependent: :destroy
-  # 課題3 いいね実装 1:Nの関係によりfavoriteに対して複数のレコードを持っているためhas_manyを使用
 
   has_many :book_comments, dependent: :destroy
-  # 課題3 コメント実装 1:Nの関係によりbook_commentに対して複数のレコードを持っているためhas_manyを使用
 
   has_one_attached :profile_image
+
+  # フォローをした、されたの関係
+  has_many :relationship, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
+  has_many :reverse_of_relationship, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
+
+  # 一覧画面で使う
+  has_many :followings, through: :relationships, source: :followed
+  has_many :followers, through: :reverse_of_relationships, source: :follower
 
   validates :name, length: { minimum: 2, maximum: 20 }, uniqueness: true
   validates :introduction, length: { maximum: 50 }
@@ -20,4 +26,20 @@ class User < ApplicationRecord
   def get_profile_image
     (profile_image.attached?) ? profile_image : 'no_image.jpg'
   end
+
+  # フォローした時の処理
+  def follow(user_id)
+    relationships.create(followed_id: user_id)
+  end
+
+  # フォローを外すときの処理
+  def unfollow(user_id)
+    relationships.find_by(followed_id: user_id).destroy
+  end
+
+  # フォローしているか判定
+  def following?(user)
+    followings.include?(user)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,21 @@
 Rails.application.routes.draw do
+  get 'relationships/followings'
+  get 'relationships/followers'
   root to: "homes#top"
   get "/home/about"=>"homes#about", as: "about"
 
   devise_for :users
-  resources :users, only: [:index,:show,:edit,:update]
+
+  # ネストさせる
+  resources :users, only: [:index,:show,:edit,:update] do
+    resource :relationships, only: [:create, :destroy]
+    get 'followings' => 'relationships#followings', as: 'followings'
+    get 'followers' => 'relationships#followers', as: 'followers'
+  end
+
   resources :books do
     resource :favorites, only:[:create, :destroy]
     resources :book_comments, only:[:create, :destroy]
-  # 課題3 いいね実装 コメント実装 ネストする　親子関係にする
-  # 課題3 いいね実装 コメント実装 投稿　削除のためcreate,destroyを使用
-  # 課題3 いいね実装 単数系resourceにすることで/:idがURLに含まれなくなる
   end
 
 end
